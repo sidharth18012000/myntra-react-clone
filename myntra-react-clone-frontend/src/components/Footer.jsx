37c6b0d7b967793b90a4073c91d1b4d83cdd4b0e
Sidharth Mahajan <sidharthmahajan74@gmail.com>
2025-03-11 21:06:32 +0530

    Change Footer & Style

--- myntra-react-clone-frontend/src/components/Footer.jsx
+++ myntra-react-clone-frontend/src/components/Footer.jsx
@@ -2,8 +2,9 @@ const Footer = () => {
   return (
     <footer>
       <div className="footer_container">
-        <div className="footer_coloumn">
-          <h3>ONLINE SHOPING</h3>
+        <div className="footer_column">
+          <h3>ONLINE SHOPPING</h3>
+
           <a href="#">Men</a>
           <a href="#">Women</a>
           <a href="#">Kids</a>
@@ -11,66 +12,100 @@ const Footer = () => {
           <a href="#">Beauty</a>
           <a href="#">Gift Cards</a>
           <a href="#">Myntra Insider</a>
+
           <h3>USEFUL LINKS</h3>
-          <a href="#">Blogs</a>
-          <a href="#">Carreer</a>
+
+          <a href="#">Blog</a>
+          <a href="#">Careers</a>
           <a href="#">Site Map</a>
-          <a href="#">Corporation Information</a>
+          <a href="#">Corporate Information</a>
           <a href="#">Whitehat</a>
           <a href="#">Cleartrip</a>
+          <a href="#">Myntra Insider</a>
         </div>
-        <div className="footer_coloumn">
+        <div className="footer_column">
           <h3>CUSTOMER POLICIES</h3>
+
           <a href="#">Contact Us</a>
           <a href="#">FAQ</a>
           <a href="#">T&C</a>
-          <a href="#">Term Of Use</a>
-          <a href="#">Track Order</a>
+          <a href="#">Terms Of Use</a>
+          <a href="#">Track Orders</a>
           <a href="#">Shipping</a>
           <a href="#">Cancellation</a>
           <a href="#">Returns</a>
           <a href="#">Privacy policy</a>
-          <a href="#">Grievance Officer</a>
+          <a href="#">Grievance Redressal</a>
         </div>
-        <div className="footer_coloumn">
+        <div className="footer_column">
           <h3>EXPERIENCE MYNTRA APP ON MOBILE</h3>
-          <div className="downloadlinks_container">
+
+          <div className="download_links">
             <a href="#">
-              <img
-                className="download_links"
-                src="public\images\Google Play.png"
-              />
+              <img src="images\Google Play.png" />
             </a>
             <a href="#">
-              <img
-                className="download_links"
-                src="public\images\App Store.png"
-              />
+              <img src="images\App Store.png" />
             </a>
-            <div className="keepin_touch">KEEP IN TOUCH</div>
+          </div>
+
+          <h3>KEEP IN TOUCH</h3>
+
+          <div className="social_media">
             <a href="#">
-              <img className="social_icon" src="public\images\facebook.png" />
-            </a>
-            <a href="#">
-              <img className="social_icon" src="public\images\instagram.png" />
+              <img src="images\facebook.png" />
             </a>
             <a href="#">
-              <img className="social_icon" src="public\images\twitter.png" />
+              <img src="images\twitter.png" />
             </a>
             <a href="#">
-              <img className="social_icon" src="public\images\youtube.png" />
+              <img src="images\youtube.png" />
+            </a>
+            <a href="#">
+              <img src="images\instagram.png" />
             </a>
           </div>
         </div>
+
+        <div className="desktop_main">
+          <div className="desktop_section">
+            <div className="desktop_original">
+              <img src="https://constant.myntassets.com/web/assets/img/6c3306ca-1efa-4a27-8769-3b69d16948741574602902452-original.png" />
+            </div>
+            <div>
+              <strong> 100% ORIGINAL </strong>
+              <span>
+                guarantee for
+                <pre>all products at myntra.com</pre>
+              </span>
+            </div>
+          </div>
+
+          <div className="desktop_section">
+            <div className="desktop_return">
+              <img src="https://assets.myntassets.com/assets/images/retaillabs/2023/5/22/becb1b16-86cc-4e78-bdc7-7801c17947831684737106127-Return-Window-image.png" />
+            </div>
+            <div>
+              <strong> Return within 14days </strong>
+              <span>
+                of
+                <pre>receiving your order</pre>
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
+
       <hr />
       <div className="info_section">
         <div className="contact">
           In case of any concern,
-          <a href="#">Contact Us</a>
+          <a href="#"> Contact Us </a>
         </div>
-        <div className="copywrite">© 2024 www.myntra.com. All rights reserved.</div>
-        <div className="flipkart">
+        <div className="copyright">
+          © 2024 www.myntra.com. All rights reserved.
+        </div>
+        <div className="flipcart">
           <a href="#">A Flipkart company</a>
         </div>
       </div>
